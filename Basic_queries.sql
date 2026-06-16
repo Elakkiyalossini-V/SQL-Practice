b7a91d166f7fa124254468eb12229b2c6f332782
Elakkiyalossini Vijayakumar <velakkiyalossini007@gmail.com>
2026-06-16 19:58:51 +0530

    Update Basic_queries.sql

--- Basic_queries.sql
+++ Basic_queries.sql
@@ -1,7 +1,11 @@
+--TO RETURN THE TOTAL REVENUE OF EACH CITY FROM THE TABLE 
+
 SELECT city,
        SUM(order_amount) AS total_revenue
 FROM orders
 GROUP BY city;
+
+--TO RETURN THE CUSTOMER NAME WHOSE ORDER AMOUNT IS GREATER THAN 1000
 
 SELECT customer_name,
        order_amount
