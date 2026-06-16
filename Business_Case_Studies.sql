d08b1540df209531363f2798edd303d66968e51e
Elakkiyalossini Vijayakumar <velakkiyalossini007@gmail.com>
2026-06-16 20:18:49 +0530

    Update Business_Case_Studies.sql

--- Business_Case_Studies.sql
+++ Business_Case_Studies.sql
@@ -1,1 +1,9 @@
+-- TOP CUSTOMERS BY REVENUE
+-- Management wants to identify customers who contribute the most revenue.
 
+SELECT customer_name,
+       SUM(order_amount) AS total_revenue
+FROM orders
+GROUP BY customer_name
+ORDER BY total_revenue DESC
+LIMIT 5;
